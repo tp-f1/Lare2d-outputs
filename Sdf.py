c06522f138b7d048c640ae8ea528c446d79048b4
tp-f1 <tgparmenter@gmail.com>
2025-02-13 10:17:12 +0000

    gif plotting

--- Sdf.py
+++ Sdf.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from pynput.keyboard import Listener, Key
+import matplotlib.animation as animation
 
 def next_image(key):
     if (key == Key.space):
@@ -19,63 +20,90 @@ listener.start()
 
 array = np.array([])
 
-starts = ["implicit", "superstepping"]
+
+starts = ["implicit"]
 
 for start in starts:
-    
-    for i in range(1000):
-        i+=100 
+    artists = []
+    fig, (ax1,ax2) = plt.subplots(1,2, sharey = True)
+    fig2, ax3 = plt.subplots()
+    artists2 = []
+    for i in range(101):
+        #i+=100 
         si = (4 -  len(str(i)))*"0" + str(i)
-        if (start == "implicit"):
-            data = sh.getdata("../../Lare2d-dev/Data/" + si + ".sdf")
-        if (start == "superstepping"):
-            data = sh.getdata("../../Lare2d-dev/Data/sh500/tri60/3Mm/" + si + ".sdf")
+        data1 = sh.getdata("../../Lare2d-dev/Data/implic-all/" + si + ".sdf")
+        data2 = sh.getdata("../../Lare2d-dev/Data/sh1000/" + si + ".sdf")
 
-        rho = data.Fluid_Rho
-        vx = data.Velocity_Vx
-        vy = data.Velocity_Vy
-        bx = data.Magnetic_Field_Bx
-        by = data.Magnetic_Field_By
-        e = data.Fluid_Energy
+        #rho = data.Fluid_Rho
+        #vx = data.Velocity_Vx
+        #vy = data.Velocity_Vy
+        #bx = data.Magnetic_Field_Bx
+        #by = data.Magnetic_Field_By
+        #e = data.Fluid_Energy
         #n = data.PIP_neutral_fraction
-        p = data.Fluid_Pressure
-        t = data.Fluid_Temperature   
-    
+        #p = data.Fluid_Pressure
+        t1 = data1.Fluid_Temperature   
+        t2 = data2.Fluid_Temperature
+
         #sh.list_variables(data)
    
-        #sh.plot_auto(e)
+        #sh.plot_auto(vy)
         #plt.show()
     
     
         #sh.plot_auto(rho)
         #plt.show()
 
-        fig, ax1 = plt.subplots()
-        ax2 = ax1.twinx()
+        #ax2 = ax1.twinx()
 
-        temp = t.data[2] * 115.3
-        density = rho.data[2] * 1.003 * 10**(-6)
-        
-        y = np.linspace(0, 170, len(temp))
+        temp1 = t1.data[2] * 115.3
+        temp2 = t2.data[2] * 115.3
+        tdiff = (temp1 - temp2) / temp2 * 100
+
+        #density = rho.data[2] * 1.003 * 10**(-6)
+        #velocity = vy.data[2] * 892 
+        time_norm = 170 * 10**6 / 892 
+        time = 10**(-4) * (i - 100) * time_norm
+        print(time)
+        y = np.linspace(0, 170, len(temp1))
         ax1.set_xlabel("Distance along loop / Mm")
-
-        ax1.plot(y,temp, color = "r")
+        
+        fig.suptitle("Radiation and heating")
+        ax1.set_title("Implicit")
         ax1.set_ylabel("Temperature / K")
         ax1.set_yscale("log")
+        plot1, = ax1.plot(y, temp1, color = "b")
+        plot2, = ax2.plot(y, temp2, color = "b")
         
-        ax2.plot(y,density, color = "g")
-        ax2.set_ylabel("Density / kgm$^-3$")
+        ax2.set_ylabel("Temperature / K")
         ax2.set_yscale("log")
+        ax2.set_xlabel("Distance along loop / Mm")
+        ax2.set_title("Superstepping")
+        
+        artists.append([plot1,plot2])
 
-        plt.show()
+        ax3.set_title("Percentage difference in temperature")
+        ax3.set_ylabel("$T_{imp}$ - $T_{super}$ / %")
+        ax3.set_xlabel("Distance along loop / Mm")
+        plot3 = ax3.plot(y, tdiff, color = "r")
+        fig2.tight_layout() 
+        artists2.append(plot3)
+        #ax2.plot(y,density, label = start)
+        #ax2.set_ylabel("Density / kgm$^-3$")
+        #ax2.set_yscale("log")
+    ani = animation.ArtistAnimation(fig = fig, artists = artists,interval = 400)
+    ani.save(filename="all.gif", writer = "pillow")
+    
+    ani2 = animation.ArtistAnimation(fig = fig2, artists = artists2, interval = 400)
+    plt.show()
+    ani2.save(filename="all-difference.gif", writer = "pillow")    
 
-        print(temp[0], temp[1], temp[2], temp[3])
+            #print(temp[0], temp[1], temp[2], temp[3])
          #plt.savefig("SH" + si + ".png")
         #plt.show()
         
         #if (i / 10 == int(i/10)):
         #plt.savefig("conduct_only" + str(i/10) + ".png") 
-plt.show()     
 #shh.plot_auto(vy)
 #t = np.linspace(0,250,num =250)
 #plt.plot(t, array)
